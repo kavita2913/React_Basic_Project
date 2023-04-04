e1180fa87d31d840985e5fd14b343bba1313e10d
kavita2913 <kavita299910@gmail.com>
2023-04-04 14:05:29 +0530

    ad

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/kavishanu293" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/kavita2913" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
